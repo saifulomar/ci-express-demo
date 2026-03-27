bb6254e6563db026afc4983b4e58b5efe74ab1a8
Saiful Omar <saifulomar@UTBs-MacBook-Pro-2.local>
2026-03-28 07:18:15 +0800

    Break test to demonstrate CI failure

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,9 +3,9 @@ const app = require('../app');
 
 describe('GET /', () => {
   it('should return CI message', async () => {
-    //const res = await request(app);
-    const res = await request(app).get('/');   // ✅ FIX HERE
-    expect(res.statusCode).toBe(200);
+    const res = await request(app).get('/'); 
+        //expect(res.statusCode).toBe(200);
+        expect(res.statusCode).toBe(201); wrong on purpose
     expect(res.body.message).toBe('CI is working!');
   });
 });
